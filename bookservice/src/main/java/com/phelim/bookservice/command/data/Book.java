47619a9bb20f8474700651a9306c9063ab572c0f
LePhuThanh <lpthanh710@gmail.com>
2024-12-27 11:10:01 +0700

    create Book entity vaf chech h2 database

---
+++ bookservice/src/main/java/com/phelim/bookservice/command/data/Book.java
@@ -0,0 +1,21 @@
+package com.phelim.bookservice.command.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "books")
+public class Book {
+    @Id
+    private String id;
+    private String name;
+    private String author;
+    private Boolean isReady;
+}
