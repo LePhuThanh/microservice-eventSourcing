e09faad9348c9df0d6e0518e8bba9adcc0d69380
LePhuThanh <lpthanh710@gmail.com>
2025-01-02 09:24:41 +0700

    Úsing AOP to custom exception which use for every service (commonservice) share libraries in Microservice Architecture (Using @Exceptionhandler & @ControlerAdvice & @ComponentScan)

---
+++ commonservice/src/test/java/com/phelim/commonservice/CommonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phelim.commonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
