5d0a4f12ecf5b7dfdeb6869b03212b6daada11bf
LePhuThanh <lpthanh710@gmail.com>
2025-01-03 10:14:48 +0700

    Handling discrupted event with try-catch & Slf4j & @DiallowReplay

--- employeeservice/src/main/java/com/phelim/employeeservice/command/event/EmployeeEventHandler.java
+++ employeeservice/src/main/java/com/phelim/employeeservice/command/event/EmployeeEventHandler.java
@@ -2,7 +2,8 @@ package com.phelim.employeeservice.command.event;
 
 import com.phelim.employeeservice.command.data.Employee;
 import com.phelim.employeeservice.command.data.EmployeeRepository;
-import jakarta.ws.rs.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.eventhandling.DisallowReplay;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+//@Slf4j
 @Component
 public class EmployeeEventHandler {
     @Autowired
@@ -24,19 +26,32 @@ public class EmployeeEventHandler {
 
     @EventHandler
     public void on(EmployeeUpdatedEvent event)throws Exception{
-        Optional<Employee> oldEmployee = employeeRepository.findById(event.getId());
-        Employee employee = oldEmployee.orElseThrow(()-> new Exception("Employee Not Found"));
+        try{
+            Optional<Employee> oldEmployee = employeeRepository.findById(event.getId());
+            Employee employee = oldEmployee.orElseThrow(()-> new Exception("Employee Not Found"));
 
-        employee.setFirstName(event.getFirstName());
-        employee.setLastName(event.getLastName());
-        employee.setKin(event.getKin());
-        employee.setDisciplined(event.getDisciplined());
-        employeeRepository.save(employee);
+            employee.setFirstName(event.getFirstName());
+            employee.setLastName(event.getLastName());
+            employee.setKin(event.getKin());
+            employee.setDisciplined(event.getDisciplined());
+            employeeRepository.save(employee);
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
     }
 
     @EventHandler
+    @DisallowReplay
     public void on(EmployeeDeletedEvent event) throws Exception{
-        employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee Not Found"));
-        employeeRepository.deleteById(event.getId());
+        try{
+            employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee Not Found"));
+            employeeRepository.deleteById(event.getId());
+        }catch(Exception ex){
+            //log.error(ex.getMessage());
+            System.out.println(ex.getMessage());
+        }
+
+
     }
 }
