64233f7f4a25143331ab0272c4e96f7cb12c9c53
LePhuThanh <lpthanh710@gmail.com>
2025-01-05 15:46:52 +0700

    crate API Gateway with Spring Cloud Gateway and config via .yml file to connect with Discovery Server

---
+++ apigateway/src/test/java/com/phelim/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phelim.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
